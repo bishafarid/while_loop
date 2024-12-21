18814c2f77c41d41c64bc2204bd64200bc4bf0fa
bisha farid <bishasiddiqui228@gmail.com>
2024-12-21 11:21:59 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const car=['bmw','volvo','saab','ford']
+let i=0;
+while(car[i]){
+    document.write('<br>',car[i]);
+    i++;
+}
+let b=0;
+while(b<=50){
+    document.write('<br>the number is even'+b)
+    b+=2;
+}
+let j=1;
+while(j<=50){
+    document.write('<br>the number is odd'+j)
+    j+=2
+}
+num=0;
+if(num>0){
+    document.write('the num is positive')
+}
+else{'enter positive num'}
